579cd5cc0e0ad85ddc3085cf827aec2d68a6891e
KraftOreo <976539567@qq.com>
2022-06-15 15:11:30 -0400

    test case

--- test.py
+++ test.py
@@ -2,8 +2,21 @@ from slingshot.src.slingshot import SlingShot
 import numpy as np
 
 env = SlingShot()
-
-for i in range(5):
-    action = np.array([0.1,0,0,0])
+print("Initialzied.")
+# pull the rubber band
+for i in range(15):
+    action = np.array([0.02,0,0,0])
     env.step(action)
-    
+    print("step", i)
+
+# release it
+action = np.array([0,0,0,0])
+env.step(action)
+action = np.array([0,0,0,-0.1])
+env.step(action)
+print("released")
+# let it fly
+for i in range(20):
+    action = np.array([0,0,0,0])
+    env.step(action)
+    print("flying", i)
